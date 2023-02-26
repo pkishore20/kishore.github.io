717386901761a25e700ad0fe903fb351c2c61210
pkishore20 <113546609+pkishore20@users.noreply.github.com>
2023-02-26 15:11:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// var loader = document.querySelector(".loader")
+
+// window.addEventListener("load", vanish);
+
+// function vanish (){
+//     loader.classList.add("disppear")
+// }
+
+// $( window ).load(function() {
+//     $(".loading").fadeOut(1000);
+//     $(".content").fadeIn(1000);
+// });
